2f89d7ca1d5f9eb5621c75c1dd075a0b68919ca2
gouthamraja22 <gouthamraja22@gmail.com>
2019-09-10 18:12:38 +0530

    jin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BannerComponent } from './banner/banner.component';
+import { IconComponent } from './icon/icon.component';
+import { ContentComponent } from './content/content.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { SignupComponent } from './signup/signup.component';
+import { FooterComponent } from './footer/footer.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BannerComponent,
+    IconComponent,
+    ContentComponent,
+    FeedbackComponent,
+    SignupComponent,
+    FooterComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
